bfb3c71fb82ba239a063dbf445c68437fab0e91d
myth <myth@ulv.io>
2020-03-25 21:45:22 +0100

    Refactor app.js

--- src/public/static/app.js
+++ src/public/static/app.js
@@ -91,175 +91,178 @@ const updateCurrent = data => {
     populationTested.innerHTML = `${(c['tested'] / c['population'] * 100).toFixed(3)} %`
 }
 
+const updateCharts = data => {
+    createChart(
+        { element: 'confirmed' },
+        [{
+            label: 'Confirmed Cases',
+            data: data['history'].map(d => d['confirmed']),
+            fill: true,
+            borderColor: YELLOW_BORDER,
+            backgroundColor: YELLOW
+        }],
+        data['history'].map(d => d['date'].slice(5, 10)),
+    )
+
+    createChart(
+        { element: 'newConfirmed' },
+        [{
+            label: 'Daily Cases',
+            data: data['history'].map(d => d['new_confirmed']),
+            fill: true,
+            borderColor: BLUE_BORDER,
+            backgroundColor: BLUE,
+        }],
+        data['history'].map(d => d['date'].slice(5, 10)),
+    )
+
+    createChart(
+        { element: 'dead' },
+        [{
+            label: 'Dead',
+            data: data['history'].map(d => d['dead']),
+            fill: true,
+            borderColor: RED_BORDER,
+            backgroundColor: RED
+        }],
+        data['history'].map(d => d['date'].slice(5, 10)),
+    )
+
+    createChart(
+        { element: 'newDead', type: 'bar' },
+        [{
+            label: 'Daily Deaths',
+            data: data['history'].map(d => d['new_dead']),
+            borderColor: RED_BORDER,
+            backgroundColor: 'rgba(209, 29, 29, 0.3)',
+            borderWidth: 1,
+        }],
+        data['history'].map(d => d['date'].slice(5, 10)),
+    )
+
+    createChart(
+        { element: 'hospitalized' },
+        [{
+            label: 'Hospitalized',
+            data: data['history'].filter(d => d['hospitalized'] > 0).map(d => d['hospitalized']),
+            fill: true,
+            borderColor: PURPLE_BORDER,
+            backgroundColor: PURPLE
+        },
+        {
+            label: 'Critical',
+            data: data['history'].filter(d => d['hospitalized'] > 0).map(d => d['hospitalized_critical']),
+            fill: true,
+            borderColor: ORANGE_BORDER,
+            backgroundColor: 'rgba(201, 78, 21, 0.5)'
+        }],
+        data['history'].filter(d => d['hospitalized'] > 0).map(d => d['date'].slice(5, 10)),
+    )
+
+    createChart(
+        { element: 'hospitalStaffInfected' },
+        [{
+            label: 'Hospital Staff Infected',
+            data: data['history'].filter(d => d['hospital_staff_infected'] > 0).map(d => d['hospital_staff_infected']),
+            fill: true,
+            borderColor: YELLOW_BORDER,
+            backgroundColor: YELLOW
+        }],
+        data['history'].filter(d => d['hospital_staff_infected'] > 0).map(d => d['date'].slice(5, 10)),
+    )
+
+    createChart(
+        { element: 'hospitalStaffQuarantined' },
+        [{
+            label: 'Hospital Staff Quarantined',
+            data: data['history'].filter(d => d['hospital_staff_quarantined'] > 0).map(d => d['hospital_staff_quarantined']),
+            fill: true,
+            borderColor: BLUE_BORDER,
+            backgroundColor: BLUE
+        }],
+        data['history'].filter(d => d['hospital_staff_quarantined'] > 0).map(d => d['date'].slice(5, 10)),
+    )
+
+    createChart(
+        { element: 'tested' },
+        [{
+            label: 'Tested',
+            data: data['history'].filter(d => d['tested'] > 0).map(d => d['tested']),
+            fill: true,
+            borderColor: GREEN_BORDER,
+            backgroundColor: GREEN
+        }],
+        data['history'].filter(d => d['hospital_staff_quarantined'] > 0).map(d => d['date'].slice(5, 10)),
+    )
+
+    // Growth factors
+
+    createChart(
+        { element: 'confirmedGrowthFactor', title: 'Total Confirmed Cases (14 day window)' },
+        [{
+            label: 'Daily Increase (%)',
+            data: data['history'].slice(
+                data['history'].length - 14, data['history'].length
+            ).map(
+                d => (d['confirmed_growth_factor'] - 1.0) * 100
+            ),
+            fill: true,
+            borderColor: ORANGE_BORDER,
+            backgroundColor: ORANGE
+        }],
+        data['history'].slice(data['history'].length - 14, data['history'].length).map(d => d['date'].slice(5, 10)),
+    )
+
+    createChart(
+        { element: 'newConfirmedGrowthFactor', title: 'New Cases (14 day window)' },
+        [{
+            label: 'Daily Change (%)',
+            data: data['history'].slice(
+                data['history'].length - 14, data['history'].length
+            ).map(
+                d => (d['new_confirmed_growth_factor'] - 1.0) * 100
+            ),
+            fill: true,
+            borderColor: BLUE_BORDER,
+            backgroundColor: BLUE
+        }],
+        data['history'].slice(data['history'].length - 14, data['history'].length).map(d => d['date'].slice(5, 10)),
+    )
+
+    createChart(
+        { element: 'hospitalGrowthFactor', title: 'Daily Hospitalization Increase (7 day window)' },
+        [{
+            label: 'Regular (%)',
+            data: data['history'].slice(
+                data['history'].length - 7, data['history'].length
+            ).map(
+                d => (d['hospitalized_growth_factor'] - 1.0) * 100
+            ),
+            fill: true,
+            borderColor: PURPLE_BORDER,
+            backgroundColor: PURPLE
+        },
+        {
+            label: 'Critical (%)',
+            data: data['history'].slice(
+                data['history'].length - 7, data['history'].length
+            ).map(
+                d => (d['hospitalized_critical_growth_factor'] - 1.0) * 100
+            ),
+            fill: true,
+            borderColor: ORANGE_BORDER,
+            backgroundColor: ORANGE
+        }],
+        data['history'].slice(data['history'].length - 7, data['history'].length).map(d => d['date'].slice(5, 10)),
+    )
+}
+
 const loadData = async() => {
     fetch('/api').then(async r => {
         const data = await r.json()
 
         updateCurrent(data)
-
-        const confirmedChart = createChart(
-            { element: 'confirmed' },
-            [{
-                label: 'Confirmed Cases',
-                data: data['history'].map(d => d['confirmed']),
-                fill: true,
-                borderColor: YELLOW_BORDER,
-                backgroundColor: YELLOW
-            }],
-            data['history'].map(d => d['date'].slice(5, 10)),
-        )
-
-        const newConfirmedChart = createChart(
-            { element: 'newConfirmed' },
-            [{
-                label: 'Daily Cases',
-                data: data['history'].map(d => d['new_confirmed']),
-                fill: true,
-                borderColor: BLUE_BORDER,
-                backgroundColor: BLUE,
-            }],
-            data['history'].map(d => d['date'].slice(5, 10)),
-        )
-
-        const deadChart = createChart(
-            { element: 'dead' },
-            [{
-                label: 'Dead',
-                data: data['history'].map(d => d['dead']),
-                fill: true,
-                borderColor: RED_BORDER,
-                backgroundColor: RED
-            }],
-            data['history'].map(d => d['date'].slice(5, 10)),
-        )
-
-        const newDeadChart = createChart(
-            { element: 'newDead', type: 'bar' },
-            [{
-                label: 'Daily Deaths',
-                data: data['history'].map(d => d['new_dead']),
-                borderColor: RED_BORDER,
-                backgroundColor: 'rgba(209, 29, 29, 0.3)',
-                borderWidth: 1,
-            }],
-            data['history'].map(d => d['date'].slice(5, 10)),
-        )
-
-        const hospitalChart = createChart(
-            { element: 'hospitalized' },
-            [{
-                label: 'Hospitalized',
-                data: data['history'].filter(d => d['hospitalized'] > 0).map(d => d['hospitalized']),
-                fill: true,
-                borderColor: PURPLE_BORDER,
-                backgroundColor: PURPLE
-            },
-            {
-                label: 'Critical',
-                data: data['history'].filter(d => d['hospitalized'] > 0).map(d => d['hospitalized_critical']),
-                fill: true,
-                borderColor: ORANGE_BORDER,
-                backgroundColor: 'rgba(201, 78, 21, 0.5)'
-            }],
-            data['history'].filter(d => d['hospitalized'] > 0).map(d => d['date'].slice(5, 10)),
-        )
-
-        const hospitalStaffInfectedChart = createChart(
-            { element: 'hospitalStaffInfected' },
-            [{
-                label: 'Hospital Staff Infected',
-                data: data['history'].filter(d => d['hospital_staff_infected'] > 0).map(d => d['hospital_staff_infected']),
-                fill: true,
-                borderColor: YELLOW_BORDER,
-                backgroundColor: YELLOW
-            }],
-            data['history'].filter(d => d['hospital_staff_infected'] > 0).map(d => d['date'].slice(5, 10)),
-        )
-
-        const hospitalStaffQuarantinedChart = createChart(
-            { element: 'hospitalStaffQuarantined' },
-            [{
-                label: 'Hospital Staff Quarantined',
-                data: data['history'].filter(d => d['hospital_staff_quarantined'] > 0).map(d => d['hospital_staff_quarantined']),
-                fill: true,
-                borderColor: BLUE_BORDER,
-                backgroundColor: BLUE
-            }],
-            data['history'].filter(d => d['hospital_staff_quarantined'] > 0).map(d => d['date'].slice(5, 10)),
-        )
-
-        const testedChart = createChart(
-            { element: 'tested' },
-            [{
-                label: 'Tested',
-                data: data['history'].filter(d => d['tested'] > 0).map(d => d['tested']),
-                fill: true,
-                borderColor: GREEN_BORDER,
-                backgroundColor: GREEN
-            }],
-            data['history'].filter(d => d['hospital_staff_quarantined'] > 0).map(d => d['date'].slice(5, 10)),
-        )
-
-        // Growth factors
-
-        const confirmedGrowthFactorChart = createChart(
-            { element: 'confirmedGrowthFactor', title: 'Total Confirmed Cases (14 day window)' },
-            [{
-                label: 'Daily Increase (%)',
-                data: data['history'].slice(
-                    data['history'].length - 14, data['history'].length
-                ).map(
-                    d => (d['confirmed_growth_factor'] - 1.0) * 100
-                ),
-                fill: true,
-                borderColor: ORANGE_BORDER,
-                backgroundColor: ORANGE
-            }],
-            data['history'].slice(data['history'].length - 14, data['history'].length).map(d => d['date'].slice(5, 10)),
-        )
-
-        const newConfirmedGrowthFactorChart = createChart(
-            { element: 'newConfirmedGrowthFactor', title: 'New Cases (14 day window)' },
-            [{
-                label: 'Daily Change (%)',
-                data: data['history'].slice(
-                    data['history'].length - 14, data['history'].length
-                ).map(
-                    d => (d['new_confirmed_growth_factor'] - 1.0) * 100
-                ),
-                fill: true,
-                borderColor: BLUE_BORDER,
-                backgroundColor: BLUE
-            }],
-            data['history'].slice(data['history'].length - 14, data['history'].length).map(d => d['date'].slice(5, 10)),
-        )
-
-        const hospitalGrowthFactorChart = createChart(
-            { element: 'hospitalGrowthFactor', title: 'Daily Hospitalization Increase (7 day window)' },
-            [{
-                label: 'Regular (%)',
-                data: data['history'].slice(
-                    data['history'].length - 7, data['history'].length
-                ).map(
-                    d => (d['hospitalized_growth_factor'] - 1.0) * 100
-                ),
-                fill: true,
-                borderColor: PURPLE_BORDER,
-                backgroundColor: PURPLE
-            },
-            {
-                label: 'Critical (%)',
-                data: data['history'].slice(
-                    data['history'].length - 7, data['history'].length
-                ).map(
-                    d => (d['hospitalized_critical_growth_factor'] - 1.0) * 100
-                ),
-                fill: true,
-                borderColor: ORANGE_BORDER,
-                backgroundColor: ORANGE
-            }],
-            data['history'].slice(data['history'].length - 7, data['history'].length).map(d => d['date'].slice(5, 10)),
-        )
+        updateCharts(data)
     })
 }
 
