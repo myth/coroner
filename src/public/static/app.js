fc19b568a2ca09020403377900e8c19cea3cb843
myth <myth@ulv.io>
2020-03-28 04:04:41 +0100

    Add demo projection chart (non-live). Add date slider for counters

--- src/public/static/app.js
+++ src/public/static/app.js
@@ -37,6 +37,15 @@ const options = {
     }
 }
 
+const setStatus = ok => {
+    const title = document.getElementById('title')
+    if (!ok) {
+        title.style.color = 'red';
+    } else {
+        title.style.color = 'unset';
+    }
+}
+
 const getLastNumDays = (data, days) => {
     size = data['history'].length
 
@@ -69,10 +78,7 @@ const setElementContent = (id, value) => {
     document.getElementById(id).innerHTML = value
 }
 
-const updateCurrent = data => {
-    const c = data['current']
-
-    setElementContent('last-update', `Updated ${data['updated']}`)
+const updateCounters = c => {
     setElementContent('counter-infected', c['infected']['total'])
     setElementContent('counter-infected-today', c['infected']['today'])
     setElementContent('counter-infected-change-percent', `${c['infected']['change_percent']} %`)
@@ -91,6 +97,38 @@ const updateCurrent = data => {
     setElementContent('counter-tested-hit-ratio', `${c['tested']['hit_ratio_percent']} %`)
     setElementContent('counter-infected-doubling-rate', `${c['infected']['doubling_rate']} days`)
     setElementContent('counter-infected-doubling-rate-ma3', `${c['infected']['doubling_rate_from_mov_avg_3']} days`)
+}
+
+const bindDatePicker = data => {
+    const history = data['history']
+    const date = document.getElementById('counter-date')
+    const slider = document.getElementById('datepicker')
+
+    const updateDate = i => {
+        date.innerHTML = history[i]['date']
+    }
+
+    updateDate(history.length - 1)
+
+    slider.setAttribute('max', `${history.length - 1}`)
+    slider.setAttribute('value', `${history.length - 1}`)
+
+    slider.oninput = function() {
+        const i = parseInt(this.value)
+
+        updateDate(i)
+        updateCounters(history[i])
+    }
+}
+
+const updateCurrent = data => {
+    setStatus(data['status'] === 'ok')
+
+    const c = data['current']
+
+    setElementContent('last-update', `Updated ${data['updated']}`)
+
+    updateCounters(c)
 }
 
 const updateCharts = data => {
@@ -203,6 +241,56 @@ const updateCharts = data => {
             backgroundColor: GREEN
         }],
         tData.map(d => d['date'].slice(5, 10)),
+    )
+
+    // Projections
+
+    const dummyCurrentData = [228, 405, 631, 805, 996, 1108, 1259, 1354, 1479, 1609, 1789, 1992, 2206, 2413, 2651, 2918, 3170, 3394, 3771, 3771]
+    const dummyData = [4262, 4607, 4978, 5375, 5800, 6255, 6742]
+    //const dummyData = [4446, 4856, 5304, 5791, 6320, 6893, 7514, 8184, 8905, 9681]
+    const avgError = 0.0347
+    const size = dummyCurrentData.length
+    const intersection = dummyCurrentData[size - 1]
+    const padding = dummyCurrentData.slice(0, size - 1).map(d => null)
+    const dummyProjectedData = [...padding, intersection, ...dummyData]
+    let n = 1
+    const dummyUpperBound = [...padding, intersection, ...dummyData.map(d => d * Math.pow((1.0 + avgError), n++))]
+    n = 1
+    const dummyLowerBound = [...padding, intersection, ...dummyData.map(d => d * Math.pow((1.0 - avgError), n++))]
+
+    let i = 1
+
+    createChart(
+        { element: 'playground', title: 'Predicted Infections (Static test data using polynomial interpolation)' },
+        [{
+            label: 'Actual',
+            data: dummyCurrentData,
+            fill: true,
+            borderColor: ORANGE_BORDER,
+            backgroundColor: ORANGE
+        },
+        {
+            label: 'Predicted',
+            data: dummyProjectedData,
+            fill: true,
+            borderColor: YELLOW_BORDER,
+            backgroundColor: YELLOW
+        },
+        {
+            label: 'Upper Bound',
+            data: dummyUpperBound,
+            fill: false,
+            borderColor: RED_BORDER,
+            backgroundColor: RED
+        },
+        {
+            label: 'Lower Bound',
+            data: dummyLowerBound,
+            fill: false,
+            borderColor: GREEN_BORDER,
+            backgroundColor: GREEN
+        }],
+        dummyProjectedData.map(d => i++)
     )
 
     // Growth factors
@@ -376,6 +464,7 @@ const loadData = async() => {
     fetch('/api').then(async r => {
         const data = await r.json()
 
+        bindDatePicker(data)
         updateCurrent(data)
         updateCharts(data)
     })
