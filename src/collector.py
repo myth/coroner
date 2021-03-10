69d5a0dd3c291dac98f2ed806909c7b77e143d57
Aleksander Skraastad <myth@overflow.no>
2021-03-10 16:09:58 +0100

    Force float type for tests.positive

--- src/collector.py
+++ src/collector.py
@@ -137,7 +137,7 @@ class Collector:
                 for a, b in zip(tests["data"], positive["data"]):
                     day = stats.setdefault(a["date"], {})
                     day["tests.today"] = a["value"]
-                    day["tests.positive"] = b["value"]
+                    day["tests.positive"] = float(b["value"])
 
                 for a, b, c in zip(hospitalized["data"], intensive_care["data"], ventilator["data"]):
                     day = stats.setdefault(a["date"], {})
